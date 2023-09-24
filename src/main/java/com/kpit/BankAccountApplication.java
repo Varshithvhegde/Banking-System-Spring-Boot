255b41c9bc8862832c4418cc47ef3e3d56ed4f50
Varshithvhegde <varshithvh@gmail.com>
2023-09-24 10:45:46 +0530

    New File

--- src/main/java/com/kpit/BankAccountApplication.java
+++ src/main/java/com/kpit/BankAccountApplication.java
@@ -2,9 +2,10 @@ package com.kpit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BankAccountApplication {
+public class BankAccountApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountApplication.class, args);
